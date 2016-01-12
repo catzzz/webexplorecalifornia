d2617532dfaf4d406a49908292577e22f19631f6
Jimmy <jimmy.leu@myflyfit.com>
2016-01-12 10:36:17 +0800

    finish angular registration

---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+// var myApp = angular.module('myApp', 
+//   ['ngRoute','firebase'])
+//   .constant('FIREBASE_URL','https://angularstarter1.firebaseio.com/');
+var myApp = angular.module('myApp',
+  ['ngRoute', 'firebase'])
+  .constant('FIREBASE_URL', 'https://angularstarter1.firebaseio.com/');
+
+myApp.run(['$rootScope','$location',function($rootScope,$location){
+  $rootScope.$on('$routeChangeError',
+    function(event,next,previous,error){
+      if(error='AUTH_REQUIRED'){
+        $rootScope.message='Sorry, you must  log in to access this page';
+        $location.path('/login');
+      }// AUTH REQUIRED
+    });// event info
+}]);// run
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'RegistrationController'
+    }).
+    when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'RegistrationController'
+    }).
+    when('/success', {
+      templateUrl: 'views/success.html',
+      controller: 'SuccessController',
+      resolve: {
+        currentAuth: function(Authentication){
+          return Authentication.requireAuth();
+        }// currentAuth
+      }//resolve
+    }).
+    otherwise({
+      redirectTo: '/login'
+    });
+}]);
---
+++ public/js/controllers/registration.js
@@ -0,0 +1,16 @@
+myApp.controller('RegistrationController',
+	['$scope','Authentication',
+	function($scope,Authentication){
+
+
+	$scope.login = function(){
+		Authentication.login($scope.user);
+	};//login
+	$scope.register=function(){
+		Authentication.register($scope.user);
+	}; //register
+	$scope.logout =function(){
+		Authentication.logout();
+	}
+}]);// Controller
+
